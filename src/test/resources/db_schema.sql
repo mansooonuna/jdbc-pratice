54045a0d044981f9d603c94798f82750b496c5fb
mansooonuna <parkdasom95@gmail.com>
2023-01-12 23:13:33 +0900

    jdbc-practice and CRUD학습

---
+++ src/test/resources/db_schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS USERS;
+
+CREATE TABLE USERS (
+    userId      VARCHAR(12)     NOT NULL,
+    password    VARCHAR(12)     NOT NULL,
+    name        VARCHAR(20)     NOT NULL,
+    email       VARCHAR(50),
+
+    PRIMARY KEY (userId)
+);
